f222a08ddc9b0617cb43df2afe6a4981291e887f
houssem <houssemgran1990@gmail.com>
2022-02-03 16:02:31 +0100

    [UPDATE] test-file

--- src/main/java/com/startU/covoiturage/common/TestFile.java
+++ src/main/java/com/startU/covoiturage/common/TestFile.java
@@ -4,10 +4,15 @@
 public class TestFile{
 
 private String username;
+private String password;
 
     public String getUsername(){
         return this.username;
     }
 
+
+    public String getPassword(){
+        return this.password;
+    }
 
 }
